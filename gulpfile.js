e548cf0a534e584beb69f2ee6f2ad467cd5a8bc6
Jacob Greenwald <jgreenwald@ITs-MacBook-Pro.local>
2017-10-25 15:11:15 -0400

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+const gulp = require("gulp"),//http://gulpjs.com/
+    util = require("gulp-util"),//https://github.com/gulpjs/gulp-util
+    sass = require("gulp-sass"),//https://www.npmjs.org/package/gulp-sass
+    autoprefixer = require('gulp-autoprefixer'),//https://www.npmjs.org/package/gulp-autoprefixer
+    minifycss = require('gulp-minify-css'),//https://www.npmjs.org/package/gulp-minify-css
+    rename = require('gulp-rename'),//https://www.npmjs.org/package/gulp-rename
+    log = util.log;
+
+gulp.task('default', function () {
+    console.log('GULP')
+})
+
+gulp.task('test', function () {
+    console.log('this is where task code goeees!!')
+})
